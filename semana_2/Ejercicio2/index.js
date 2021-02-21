11e74a989e6fb723fd6c927e8b6f19aff52d657b
Do95 <haku@Dev.local>
2021-02-21 15:06:17 -0600

    Semana 2

---
+++ semana_2/Ejercicio2/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  let texto = 'Happy Punk',
+	textoReversa = texto.split('').reverse().join('');
+  res.send(textoReversa);
+});
+app.listen(PORT, HOST);
